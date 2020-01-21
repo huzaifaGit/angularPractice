79188a044de6b46e9f9e4270dc09ed2403a52efd
Huzaifa Khan <53226990+huzaifaGit@users.noreply.github.com>
2020-01-21 17:56:06 +0500

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice'},
+    { id: 12, name: 'Ozil' },
+    { id: 13, name: 'Ronaldo7' },
+    { id: 14, name: 'Ceballos' },
+    { id: 15, name: 'Flash' },
+    { id: 16, name: 'Wayne' },
+    { id: 17, name: 'KDB' },
+    { id: 18, name: 'Sane' },
+    { id: 19, name: 'Python' },
+    { id: 20, name: 'Dybala'} 
+];
